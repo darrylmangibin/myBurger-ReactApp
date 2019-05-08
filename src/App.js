79ec823db19da411be8ba5df1dd085d2b540be35
Darryl <darrylmangibin@gmail.com>
2019-05-08 21:54:07 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import Layout from './components/Layout/Layout';
-import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 function App() {
   return (
     <div>
-      <Layout>
-        <BurgerBuilder />
-      </Layout>
+      App
     </div>
   );
 }
--- src/hoc/Auxiliary.js
+++
@@ -1,5 +0,0 @@
-const Auxiliary = (props) => {
-  return props.children
-}
-
-export default Auxiliary;
